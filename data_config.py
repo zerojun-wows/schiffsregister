d0a2906957d1d9157c7047f2c1652e2dd35b9d09
zerojun-wows <zerojun@gmx.net>
2024-11-17 22:43:15 +0100

    Auto sync commit

---
+++ data_config.py
@@ -0,0 +1,22 @@
+"""
+Dieses Modul enthält die Daten und Strukturkonfigurationen des Schiffsregisters
+"""
+import streamlit as st
+
+nations_order_dict = {
+    "JAPAN": 1,
+    "USA": 2,
+    "UDSSR": 3,
+    "DEUTSCHLAND": 4,
+    "GB": 5,
+    "FRANKREICH": 6,
+    "ITALIEN": 7,
+    "PANASIEN": 8,
+    "EUROPA": 9,
+    "DIE NIEDERLANDE": 10,
+    "COMMONWEALTH": 11,
+    "PANAMERIKA": 12,
+    "SPANIEN": 13,
+}
+
+type_option_list = ["standard", "elite", "premium", "spezial"]
