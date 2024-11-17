04a50f44362388ddc81debe34a1dfba7253db183
zerojun-wows <zerojun@gmx.net>
2024-11-17 19:25:21 +0100

    Auto sync commit

--- schiffsregister.py
+++ schiffsregister.py
@@ -5,7 +5,26 @@ import streamlit as st
 
 
 def main():
-    
+    st.title("Schiffsregister")
 
-if __name__ == '__main__':
+    st.header("Neues oder bestehendes Schiffsregister")
+
+    st.header("Aktueller Schiffsbestand")
+
+    st.subheader("Zugänge")
+
+    st.subheader("Änderungen")
+
+    st.subheader("Abgänge")
+
+    st.header("Registerbearbeitung")
+
+    st.subheader("Schiff hinzufügen")
+
+    st.subheader("Schiff bearbeiten")
+
+    st.subheader("Schiff entfernen")
+
+
+if __name__ == "__main__":
     main()
