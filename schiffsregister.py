415332c983571cd3763201a509f934bf659ed28f
zerojun-wows <zerojun@gmx.net>
2024-11-21 06:19:42 +0100

    Auto sync commit

--- schiffsregister.py
+++ schiffsregister.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-pages = {"Schiffsregister": [st.Page("show_register.py", title="Ansehen")]}
+pages = {
+    "Schiffsregister": [
+        st.Page("show_register.py", title="Ansehen"),
+        st.Page("open_register.py", title="Öffnen"),
+    ]
+}
 
 pg = st.navigation(pages)
 pg.run()
