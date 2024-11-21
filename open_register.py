1470a5f61e1dc4a6166558d003658e5622968ebf
zerojun-wows <zerojun@gmx.net>
2024-11-21 14:43:05 +0100

    Auto sync commit

--- open_register.py
+++ open_register.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("Schiffsregister - Öffnen")
 
 st.write(
-    f"Sie können ein neues leeres Schiffsregister eröffnen, oder"
+    f"Sie können ein neues leeres Schiffsregister eröffnen, oder "
     f"ein bestehendes Schiffsregister hochladen."
 )
 
