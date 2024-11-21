7a9c374b5e0cca170a88d879098b998325d80dfa
zerojun-wows <zerojun@gmx.net>
2024-11-21 17:47:53 +0100

    Auto sync commit

--- dump_register.py
+++ dump_register.py
@@ -1,17 +1,9 @@
 import streamlit as st
 
+from session_state_routines import check_register_open
+
 st.title("Schiffsregister - Ausgeben")
 
-if len(st.session_state) == 0:
-    st.info(
-        f"Sie müssen zuerst ein Schiffsregister öffnen.",
-        icon=":material/info:",
-    )
-    st.page_link(
-        "open_register.py",
-        label="Schiffsregister - Öffnen",
-        icon=":material/file_open:",
-    )
-    st.stop()
+check_register_open()
 
 st.write(st.session_state)
--- show_register.py
+++ show_register.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from session_state_routines import check_register_open
 
 st.title("Schiffsregister - Ansicht")
 
