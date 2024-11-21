47ac5d865f40bf3b50813df2c524b2cbbc65423b
zerojun-wows <zerojun@gmx.net>
2024-11-21 18:16:12 +0100

    Auto sync commit

--- open_register.py
+++ open_register.py
@@ -19,6 +19,16 @@ if st.button("Neues Schiffsregister eröffnen"):
                 "Geöffnetes Schiffsregister wird entfernt und ein leeres Schiffsregister wird eröffnet",
             ],
         )
+        if override_request == "Ja":
+            # session_state löschen
+            # variablen im Session_state setzen
+        else
+            st.info("Kein neues Schiffsregister eröffnet.", icon=":material/info:",)
+            
+            st.info(
+                "Das bereits geöffnete Schiffsregister kann weiterhin genutzt werden.",
+                icon=":material/info:",
+            )
 
 
 st.write(st.session_state)
