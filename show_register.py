a8ebc0c21d95ad5e1a5be29c35d8cfffa234ea71
zerojun-wows <zerojun@gmx.net>
2024-11-21 22:01:47 +0100

    Auto sync commit

--- show_register.py
+++ show_register.py
@@ -5,4 +5,8 @@ st.title("Schiffsregister - Ansicht")
 
 check_register_open()
 
+st.subheader("Aktueller Bestand")
+
+current_ship_register_df = get_current_ship_register_dataframe()
+
 st.write(st.session_state)
