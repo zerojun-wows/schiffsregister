6b4b938d09325090dbfb579991bd0811bc94db7e
zerojun-wows <zerojun@gmx.net>
2024-11-21 17:48:57 +0100

    Auto sync commit

--- show_register.py
+++ show_register.py
@@ -3,16 +3,6 @@ from session_state_routines import check_register_open
 
 st.title("Schiffsregister - Ansicht")
 
-if len(st.session_state) == 0:
-    st.info(
-        f"Sie müssen zuerst ein Schiffsregister öffnen.",
-        icon=":material/info:",
-    )
-    st.page_link(
-        "open_register.py",
-        label="Schiffsregister - Öffnen",
-        icon=":material/file_open:",
-    )
-    st.stop()
+check_register_open()
 
 st.write(st.session_state)
