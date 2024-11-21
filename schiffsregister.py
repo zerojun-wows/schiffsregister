7595b0b380aaf305fc2a42ba83878d3296a27ade
zerojun-wows <zerojun@gmx.net>
2024-11-21 07:16:42 +0100

    Auto sync commit

--- schiffsregister.py
+++ schiffsregister.py
@@ -2,11 +2,13 @@ import streamlit as st
 
 pages = {
     "Schiffsregister": [
-        st.Page("show_register.py", title="Ansehen"),
+        st.Page("show_register.py", title="Ansehen", icon=":material/news:"),
         st.Page(
             "open_register.py", title="Öffnen", icon=":material/file_open:"
         ),
-        st.Page("dump_register.py", title="Ausgeben"),
+        st.Page(
+            "dump_register.py", title="Ausgeben", icon=":material/file_save:"
+        ),
     ],
     # "Bearbeitungsoptionen": []
 }
