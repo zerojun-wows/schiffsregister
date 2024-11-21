d191d52ae7e9b52539879c359e01288e7a46ae0f
zerojun-wows <zerojun@gmx.net>
2024-11-21 06:31:41 +0100

    Auto sync commit

--- dump_register.py
+++ dump_register.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
 st.title("Schiffsregister - Ausgeben")
+
+if len(st.session_state) == 0:
+    st.info(
+        "Damit etwas angezeigt werden kann muss zunächst ein Schiffsregister geöffnet werden."
+    )
