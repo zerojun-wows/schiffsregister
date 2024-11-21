b797bd284edd7661f32c92fc81912e7c67d4cf65
zerojun-wows <zerojun@gmx.net>
2024-11-21 18:06:36 +0100

    Auto sync commit

--- open_register.py
+++ open_register.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from session_state_routines import is_session_state_empty
 
 st.title("Schiffsregister - Öffnen")
 
@@ -8,8 +9,20 @@ st.write(
 )
 
 if st.button("Neues Schiffsregister eröffnen"):
+    if not is_session_state_empty:
+        st.warning("Ein Schiffsregister ist bereits geöffnet")
+        override_request = st.radio(
+            "Wollen Sie das bestehende Schiffsregister überschreiben?",
+            ["Nein", "Ja"],
+            caption=[
+                "Es wird kein neues Schiffsregister eröffnet",
+                "Geöffnetes Schiffsregister wird entfernt und ein leeres Schiffsregister wird eröffnet",
+            ],
+        )
+    # Bei vorhanden Daten sicherheitsabfrage
     # clear session_state
     # set session_state variables
     pass
+
 
 st.write(st.session_state)
