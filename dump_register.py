ad6616a230f22f0992ea68d947b7509166156218
zerojun-wows <zerojun@gmx.net>
2024-11-21 06:49:10 +0100

    Auto sync commit

--- dump_register.py
+++ dump_register.py
@@ -4,6 +4,7 @@ st.title("Schiffsregister - Ausgeben")
 
 if len(st.session_state) == 0:
     st.info(
-        f'Sie müssen zuerst ein Schiffsregister {st.page_link("open_register.py", label="öffnen")}.'
+        f"Sie müssen zuerst ein Schiffsregister öffnen."
+        st.page_link("open_register.py", label="Öffnen")
     )
     st.stop()
