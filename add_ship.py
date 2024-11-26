24313af79faf245902259344e9d484e504d321d5
zerojun-wows <zerojun@gmx.net>
2024-11-26 19:39:09 +0100

    Auto sync commit

--- add_ship.py
+++ add_ship.py
@@ -15,7 +15,13 @@ with st.form("add_ship_form"):
     nation = st.selectbox(
         "Nation",
         options=nations_order_dict.keys(),
-        key="new_ship_nation",
+        key="add_ship_nation",
     )
-    typ = st.selectbox("Typ", options=type_option_list, key="new_ship_type")
+    typ = st.selectbox("Typ", options=type_option_list, key="add_ship_type")
+    klasse = st.selectbox(
+        "Klasse",
+        options=ship_class_order_dict.keys(),
+        key="add_ship_class",
+    )
+
     submitted = st.form_submit_button("Schiff hinzufügen")
