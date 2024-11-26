b62c7d12bba50fa7f606923e8caad00604abad4e
zerojun-wows <zerojun@gmx.net>
2024-11-26 19:40:18 +0100

    Auto sync commit

--- add_ship.py
+++ add_ship.py
@@ -2,6 +2,7 @@ import streamlit as st
 from data_config import (
     nations_order_dict,
     type_option_list,
+    class_order_dict,
 )
 from session_state_routines import (
     check_register_open,
@@ -20,7 +21,7 @@ with st.form("add_ship_form"):
     typ = st.selectbox("Typ", options=type_option_list, key="add_ship_type")
     klasse = st.selectbox(
         "Klasse",
-        options=ship_class_order_dict.keys(),
+        options=class_order_dict.keys(),
         key="add_ship_class",
     )
 
