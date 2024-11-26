83c196fb5c11d2544f943a3ebe7ed1dc15ee6f2b
zerojun-wows <zerojun@gmx.net>
2024-11-26 02:39:38 +0100

    Auto sync commit

--- modify_ship.py
+++ modify_ship.py
@@ -4,6 +4,6 @@ from session_state_routines import (
     check_register_open,
 )
 
-st.title("Schiffsregister - Schiff bearbeiten")
+st.title("Schiffsregister - Schiff entfernen")
 
 check_register_open()
