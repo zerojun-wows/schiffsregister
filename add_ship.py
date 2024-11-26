26dc58ec67cf494e0c9f3bac97ef4625fa1a19e1
zerojun-wows <zerojun@gmx.net>
2024-11-26 22:48:04 +0100

    Auto sync commit

--- add_ship.py
+++ add_ship.py
@@ -55,7 +55,7 @@ with st.form("add_ship_form"):
     submitted = st.form_submit_button("Schiff hinzufügen")
     reset = st.form_submit_button("Zurücksetzen", on_click=reset_add_form)
 
-    if all(submitted, not nation, not typ, not klasse, not stufe):
+    if all([submitted, not nation, not typ, not klasse, not stufe]):
         st.error(
             "In einem der Auswahlfelder fehlt die Auswahl",
             icon=":material/error:",
@@ -67,7 +67,7 @@ with st.form("add_ship_form"):
             icon=":material/error:",
         )
 
-    if all(submitted, name, nation, typ, klasse, stufe):
+    if all([submitted, name, nation, typ, klasse, stufe]):
         add_ship = {
             "Nation": nation,
             "Typ": typ,
