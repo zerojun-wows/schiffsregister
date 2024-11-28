4675f2badc7f81b1220411f5598e20605aa8a6a0
zerojun-wows <zerojun@gmx.net>
2024-11-28 01:41:12 +0100

    Auto sync commit

--- modify_ship.py
+++ modify_ship.py
@@ -2,6 +2,7 @@ import streamlit as st
 
 from session_state_routines import (
     check_register_open,
+    get_current_ship_register_list,
 )
 
 st.title("Schiffsregister - Schiff bearbeiten")
@@ -10,8 +11,8 @@ check_register_open()
 
 select_edit_index = st.selectbox(
     "Auswahl des zu bearbeitenden Schiffes",
-    options=range(len(st.session_state.schiffsregister_aktuell)),
-    format_func=lambda i: st.session_state.schiffsregister_aktuell[i]["Name"],
+    options=range(len(get_current_ship_register_list())),
+    format_func=lambda i: get_current_ship_register_list()[i]["Name"],
 )
 
 st.write(st.session_state)
