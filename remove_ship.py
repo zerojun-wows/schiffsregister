3ad15eb48152c77392a99b7198e7b26afd19cc47
zerojun-wows <zerojun@gmx.net>
2024-12-04 06:26:27 +0100

    Auto sync commit

--- remove_ship.py
+++ remove_ship.py
@@ -14,3 +14,19 @@ if not get_current_ship_register_list():
         icon=":material/warning:",
     )
     st.stop()
+
+selected_remove_index = st.selectbox(
+    "Auswahl des zu entfernenden Schiffes",
+    options=range(len(get_current_ship_register_list())),
+    index=None,
+    format_func=lambda i: get_current_ship_register_list()[i]["Name"],
+    placeholder="Bitte eine Auswahl treffen!",
+)
+
+remove_button = st.button("Gewähltes Schiff entfernen")
+
+if edit_button and selected_edit_index is None:
+    st.error(
+        "Die Auswahl eines Schiffes ist erforderlich!",
+        icon=":material/error:",
+    )
