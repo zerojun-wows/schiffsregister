30dd5b3459e969a91b60edb67393b6f22a1d9a34
zerojun-wows <zerojun@gmx.net>
2024-12-05 07:23:59 +0100

    Auto sync commit

--- remove_ship.py
+++ remove_ship.py
@@ -21,11 +21,23 @@ selected_remove_index = st.selectbox(
     options=range(len(get_current_ship_register_list())),
     index=None,
     format_func=lambda i: get_current_ship_register_list()[i]["Name"],
-    # key="remove_selected_ship"
+    key="remove_selected_index"
     # on_change=,
     placeholder="Bitte eine Auswahl treffen!",
 )
 
+if selected_remove_index is not None and selected_remove_index >= 0:
+    selected_ship_data = get_current_ship_register_list()[selected_remove_index]
+    st.html(
+        "<table width='100%'><tr>"
+        "<th>Nation</th><th>Typ</th><th>Klasse</th><th>Stufe</th><th>Name</th>"
+        "</tr><tr>"
+        f"<td>&emsp;{selected_ship_data['Nation']}</td>"
+        f"<td>&emsp;{selected_ship_data['Typ']}</td>"
+        f"<td>&emsp;{selected_ship_data['Klasse']}</td>"
+        f"<td>&emsp;{selected_ship_data['Stufe']}</td>"
+        f"<td>&emsp;{selected_ship_data['Name']}</td>"
+    )
 
 # remove_button = st.button("Gewähltes Schiff entfernen")
 
