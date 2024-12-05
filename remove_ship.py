4d620a2340c20c46cc691e2b0b38d03ff5bd6d17
zerojun-wows <zerojun@gmx.net>
2024-12-05 07:32:53 +0100

    Auto sync commit

--- remove_ship.py
+++ remove_ship.py
@@ -41,6 +41,11 @@ if selected_remove_index is not None and selected_remove_index >= 0:
 
     remove_button = st.button("Gewähltes Schiff entfernen")
 
+    if remove_button:
+        st.write(
+            f"Wollen sie das Schiff {selected_ship_data['Name']} endgültig entfernen?"
+        )
+
 # if remove_button and selected_remove_index is None:
 #     st.error(
 #        "Die Auswahl eines Schiffes ist erforderlich!",
