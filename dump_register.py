9b04a8322f03baa49d6b80bd936d216a8503a803
zerojun-wows <zerojun@gmx.net>
2024-12-11 09:33:07 +0100

    Auto sync commit

--- dump_register.py
+++ dump_register.py
@@ -1,9 +1,27 @@
 import streamlit as st
-
-from session_state_routines import check_register_open
+import time
+from session_state_routines import (
+    check_register_open,
+    get_current_ship_register_dataframe,
+)
 
 st.title("Schiffsregister - Ausgeben")
 
 check_register_open()
+
+st.subheader("Schiffsregister download")
+
+csv = (
+    get_current_ship_register_dataframe()
+    .to_csv(columns=main_columns, index=False)
+    .encode("utf-8")
+)
+
+st.download_button(
+    label="Aktuelles Schiffsregister als CSV-Datei herunterladen",
+    data=csv,
+    file_name=f'{time.strftime("%Y_%m_%d-%H_%M")}-Schiffsregister.csv',
+    mime="text/csv",
+)
 
 st.write(st.session_state)
