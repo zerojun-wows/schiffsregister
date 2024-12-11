77268106b3632ea4421dac83f8cd444031e59804
zerojun-wows <zerojun@gmx.net>
2024-12-11 08:38:40 +0100

    Auto sync commit

--- session_state_routines.py
+++ session_state_routines.py
@@ -44,6 +44,8 @@ def get_additions_dataframe() -> pd.Dataframe:
         original_register_df.index
     )
 
+    return current_register_df.loc[new_indices]
+
 
 def get_current_ship_register_dataframe() -> pd.DataFrame:
     return pd.DataFrame(
